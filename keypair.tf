a2f2db0c180094f28663efc6d2c2dabecb6aeb16
Chetan <50010056@mahindra.com>
2025-02-10 16:54:19 +0530

    modified file

--- keypair.tf
+++ keypair.tf
@@ -1,4 +1,4 @@
 resource "aws_key_pair" "dev-key" {
   key_name   = "dev-key"
-  public_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIO3z5FvrcX70JGxsYLy60olK5kkU2rYDRrgMga3vB+t6 User@Chetan"
+  public_key = "your public key"
 }
